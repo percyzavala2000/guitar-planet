f939e5655c086f7531b599f6c19f4d686bb17d2f
percyzavala <percyzavala@gmail.com>
2024-06-08 02:43:06 -0500

    se implemento el useCart.tsx se traslado todo las funciones de componente app

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,13 @@
-import { useEffect, useState } from 'react';
+
 import Guitar from './components/Guitar';
 import Header from './components/Header';
-import { database } from './database/data';
-import { Datos, DatosItem } from './interfaces/interfaces';
+
+import { useCart } from './hooks/useCart';
 
 
 
 function App() {
-const initialCart=()=>{
-  const localStorageCart=localStorage.getItem('cart');
-  return localStorageCart? JSON.parse(localStorageCart):[];
-}
-const [data] = useState<Datos[]>(database);
-const [cart, setCart] = useState<DatosItem []>(initialCart);
-const MAX_ITEMS=10;
-const MIN_ITEMS=0;
-
-useEffect(() => {
-  
-  localStorage.setItem('cart',JSON.stringify(cart));
-  
-}, [cart])
-
-
-const addToCart = (item:DatosItem) => { 
-  const itemExiste=cart.findIndex(e=>e.id===item.id);
-  const updateCart=[...cart];
-  if(itemExiste>=0){
-    updateCart[itemExiste].quantity++;
-    setCart(updateCart);
-    console.log("esxiste guitarra")
-    
-  }else{
-    console.log("se agrega no existe")
-    item.quantity=1;
-    setCart([...cart,item]);
-
-  }
-  
- }
-
- const removerFromCart = (id:Datos['id']) => {
-  console.log("eliminando",id);
-  setCart(prevCart=>prevCart.filter(datos=>datos.id!==id));
-  
- }
-
- const incrementarQuantity = (id:Datos['id']) => {
-
-const updateCart=cart.map(item=>{
-if(item.id===id&&item.quantity<MAX_ITEMS){
-  return{
-    ...item,quantity:item.quantity+1
-  }
-
-}
-return item;
-
-})
-setCart(updateCart);
-
- }
- const decrementarQuantity = (id:Datos['id']) => {
-  const updateCart=cart.map(item=>{
-    if(item.id===id&&item.quantity>MIN_ITEMS){
-      return{
-        ...item,quantity:item.quantity-1
-      }
-    
-    }
-    return item;
-    
-    })
-    setCart(updateCart);
- }
-const clearCart = () => {
-  console.log("vaciando carrito");
-  setCart([]);
-  
-}
-
+  const {data,cart,addToCart,removerFromCart,incrementarQuantity,decrementarQuantity,clearCart}=useCart();
 
   return (
     <>
