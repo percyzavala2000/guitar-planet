0a2218b443bbce6112cefc5e0342ce658cbcff6c
percyzavala <percyzavala@gmail.com>
2024-06-07 23:56:31 -0500

    Se agrego compenentes Guitar,Header;database:data.ts y interfeces:interface.ts

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,25 @@
+export type Datos = {
+  id: number,
+  name: string,
+  image: string,
+  description: string,
+  price:number,
+  quantity?: number,
+}
+
+export type DatosProps={
+
+  data:DatosItem,
+  addToCart:(item:DatosItem)=>void;
+}
+
+export type DatosItem={
+  id: number,
+  name: string,
+  image: string,
+  description: string,
+  price:number
+  quantity:number
+ 
+  
+}
